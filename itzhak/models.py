1cd2b19e2f02779e2f958341e2748438c4a0a29b
sidiousvic <sidiousvic@gmail.com>
2021-07-04 18:44:17 +0900

    Add User model

--- itzhak/models.py
+++ itzhak/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import base_user
 
-# Create your models here.
+class User(base_user.AbstractBaseUser): 
+    """
+    A user of Itzhak. A user can perform CRUD actions around expenses,
+    profile, and budgets. 
+    """
+
+    email = models.CharField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    balance = models.FloatField(default=0, blank=True) 

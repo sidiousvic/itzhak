15bee2a761449baba208b3af6c615037f904ef38
sidiousvic <sidiousvic@gmail.com>
2021-07-04 17:56:08 +0900

    Initialize Itzhak app

--- itzhak_project/asgi.py
+++ itzhak_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for itzhak project.
+ASGI config for itzhak_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'itzhak.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'itzhak_project.settings')
 
 application = get_asgi_application()
